2159d2fc94f181baf8d941d4666995da2a59d4b5
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-07-28 08:02:01 +0500

    Добавьте файлы проекта.

---
+++ learning_csharp_202507/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace learning_csharp_202507
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
