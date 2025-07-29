d1ce96656423da421bef8a818477bc6551e0452c
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-07-29 16:05:37 +0500

    Начал выносить валидаторы в отдельный файл (класс)

---
+++ user_profile/Validators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_profile
+{
+    public static class Validators
+    {
+        public static bool IsValidInput(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+        public static bool IsValidAge(string inputAge, out int age)
+        {
+            return int.TryParse(inputAge, out age) && age > 0 && age < 130;
+        }
+        public static bool IsValidEmail(string email)
+        {
+            return (email.Count(c => c == '@') == 1) && email.Any(c => c == '.') && email.Length > 4;
+        }
+    }
+}
