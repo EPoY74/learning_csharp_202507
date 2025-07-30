5cd6dd65007855bb1c13594f04b86e2a7d119032
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-07-31 02:07:08 +0500

    Сохранение, закоммитится или нет???

---
+++ user_profile/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace user_profile.Models
+{
+    public class User
+    {
+        // объявляем атрибуты экземплара класса
+        public string Name { get; }
+        public int Age { get; }
+        public string Email { get; }
+
+
+        // ИНициализинуем экземпляр
+        public User(string name, int age, string email)
+        {
+            Name = name;
+            Age = age;
+            Email = email;
+
+        }
+        //Переопределяем ToString - можно сделать красивый вывод, как мы хотим
+        public override string ToString()
+        {
+            return $"Имя: {Name}, Возраст {Age}, E-mail: {Email}";
+        }
+    }
+}
