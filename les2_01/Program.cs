6a1c1a5a3c5cfb789155bd821b45fab0698cf2ed
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-08-09 21:49:25 +0500

    Testing email validation using MailAddress(email)

---
+++ les2_01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Mail;
+
+namespace les2_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Введите электронную почту: ");
+                string? inputEmail = Console.ReadLine();
+                if ((inputEmail is not null) && (inputEmail.Length > 5))
+                {
+                    string emailAddr = string.Empty;
+                    string email = inputEmail.Trim();
+                    try
+                    {
+                        
+                        emailAddr = new MailAddress(email).Address; // Проверка корректности email
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Почта ввредена не в корректном формате. ");
+                        continue; // Возвращаемся к началу цикла, чтобы запросить email снова
+                    }
+
+                    if (emailAddr.Length > 5)
+                    {
+                        Console.WriteLine($"Электронная почта {emailAddr} была введена корректно.");
+                        break; // Выходим из цикла, если email введен корректно
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы не ввели электронную почту.");
+                    continue; // Возвращаемся к началу цикла, чтобы запросить email снова
+                }
+            }
+
+        }
+    }
+}
